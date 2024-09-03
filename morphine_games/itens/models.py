f1de910bf30769f02f8dac528809eab7d740112a
Kanekinha <160861366+Kaneka8@users.noreply.github.com>
2024-09-03 12:22:12 -0300

    Update models.py

--- morphine_games/itens/models.py
+++ morphine_games/itens/models.py
@@ -8,18 +8,18 @@ class Client(models.Model):
         self.money = 1000000000.00  # Fortuna do Luciano Hang
         super().save(*args, **kwargs)
 
-class Product(models.Model):
+class Produtos(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
 
 class Carrinho(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    produtos = models.ManyToManyField(Product, through='CarrinhoProduto')
+    produtos = models.ManyToManyField(Produtos, through='CarrinhoProduto')
 
 class CarrinhoProduto(models.Model):
     carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Product, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produtos, on_delete=models.CASCADE)
     quantidade = models.IntegerField()
 
     def save(self, *args, **kwargs):
