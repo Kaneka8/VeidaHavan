e34681115775fce0068626ab699c0c4d22cedc68
Kanekinha <160861366+Kaneka8@users.noreply.github.com>
2024-09-17 13:19:36 -0300

    ultimo

--- morphine_games/itens/models.py
+++ morphine_games/itens/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Produtos(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.CharField(max_length=255)
+
--- morphine_games/itens/views.py
+++ morphine_games/itens/views.py
@@ -4,13 +4,24 @@ from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+import json
+
+from itens.models import Produtos
+
 @method_decorator(csrf_exempt, name='dispatch')
 class ItensView(View):
     def get(self, request):
         return HttpResponse("Morphine chamou o GET")
     
     def post(self, request):
-        return HttpResponse("Morphine chamou o POST")
+        data = json.loads(request.body)
+        item = Produtos(
+            name=data.get('name'),
+            price=data.get('price'), 
+            image_url=data.get('image_url')
+        )
+        item.save()
+        return HttpResponse("Item created successfully!")
     
     def put(self, request):
         return HttpResponse("Morphine chamou o PUT")
