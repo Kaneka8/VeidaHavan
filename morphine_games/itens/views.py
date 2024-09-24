265a9600da9056f88fc3596cb7f59e772548feb7
Kanekinha <160861366+Kaneka8@users.noreply.github.com>
2024-09-24 11:43:51 -0300

    vai

--- morphine_games/itens/views.py
+++ morphine_games/itens/views.py
@@ -22,23 +22,31 @@ class ItensView(View):
         )
         item.save()
         return HttpResponse("Item created successfully!")
-    
-def put(self, request):
-    data = json.loads(request.body)
-    item_id = data.get('id')
-    if item_id:
-        try:
-            item = Produtos.objects.get(id=item_id)
-            item.name = data.get('name', item.name)
-            item.price = data.get('price', item.price)
-            item.image_url = data.get('image_url', item.image_url)
-            item.save()
-            return HttpResponse("Item updated successfully!")
-        except Produtos.DoesNotExist:
-            return HttpResponseNotFound("Item not found")
-    else:
-        return HttpResponseBadRequest("Item ID is required")
+    def put(self, request):
+        data = json.loads(request.body)
+        item_id = data.get('id')
+        if item_id:
+            try:
+                item = Produtos.objects.get(id=item_id)
+                item.name = data.get('name', item.name)
+                item.price = data.get('price', item.price)
+                item.image_url = data.get('image_url', item.image_url)
+                item.save()
+                return HttpResponse("Item updated successfully!")
+            except Produtos.DoesNotExist:
+                return HttpResponseNotFound("Item not found")
+        else:
+            return HttpResponseBadRequest("Item ID is required")
     
     def delete(self, request):
-        return HttpResponse("Morphine chamou o DELETE")
-    
+        data = json.loads(request.body)
+        item_id = data.get('id')
+        if item_id:
+            try:
+                item = Produtos.objects.get(id=item_id)
+                item.delete()
+                return HttpResponse("Item deleted successfully!")
+            except Produtos.DoesNotExist:
+                return HttpResponseNotFound("Item not found")
+        else:
+            return HttpResponseBadRequest("Item ID is required")
